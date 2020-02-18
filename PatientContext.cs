de17579f1b9fc1fa78e783caa02e46cb654e4f8d
Kanokchead-Code <61183407+Kanokchead-Code@users.noreply.github.com>
2020-02-18 17:13:27 +0700

    Add files via upload

---
+++ PatientContext.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data;
+
+
+namespace ConnectString.Models
+{
+    public class PatientContext
+    {
+        public string ConnectionString { get; set; }
+
+        public PatientContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<Patient> GetAllPatient()
+        {
+            List<Patient> list = new List<Patient>();
+            
+            using (MySqlConnection conn = GetConnection())
+            {
+                if(conn.State != System.Data.ConnectionState.Open)
+                conn.Open();
+                
+                MySqlCommand cmd = new MySqlCommand("Select * from patient",conn);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                { while (reader.Read())
+                    {
+
+                        list.Add(new Patient()
+                        {
+                            Patient_id = reader["pat_id"].ToString(),
+                            Patient_Name = reader["pat_name"].ToString(),
+                            Patient_sex =  reader["pat_sex"].ToString()
+                        });
+                        
+                    }
+                        
+                }
+                
+            }
+            return list;
+        }
+
+    }
+}
---
+++ PictureContext.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectString.Models
+{
+    public class PictureContext
+    {
+        public string ConnectionString2 { get; set; }
+
+        public PictureContext(string connectionString)
+        {
+            this.ConnectionString2 = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString2);
+        }
+        public List<Picture> GetAllPicture()
+        {
+            List<Picture> pic = new List<Picture>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT pat_id, study_iuid, pat_name, " +
+                    "study_desc, concat('http://192.168.2.10:8080/wado/wado?requestType=WADO&studyUID=' , study_iuid , '&seriesUID=' , study_iuid , '&objectUID=' , study_iuid ,  '&contentType=image/jpeg') as url FROM patient AS p JOIN study AS s ON p.pk = s.pk", conn);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                {
+                    while (reader.Read())
+
+                        pic.Add(new Picture()
+                        {
+                            P_ID = reader["pat_id"].ToString(),
+                            PPic_ID = reader["study_iuid"].ToString(),
+                            PPic_Name = reader["pat_name"].ToString(),
+                            PPic_Case = reader["study_desc"].ToString(),
+                            PPic_url = reader["url"].ToString()
+
+                        }) ;
+
+                }
+
+            }
+            return pic;
+        }
+
+    }
+}
